4f92612cc8b6c16bc3715cc9565489772cf7e3fc
Jason <71078248+JsonMack@users.noreply.github.com>
2021-05-21 18:32:46 -0300

    Removed unused prints from unit tests.

--- src/test/java/com/jsonmack/datastructures/singly_linkedlist/NodeTest.java
+++ src/test/java/com/jsonmack/datastructures/singly_linkedlist/NodeTest.java
@@ -3,6 +3,11 @@ package com.jsonmack.datastructures.singly_linkedlist;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayDeque;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 class NodeTest {
@@ -13,18 +18,18 @@ class NodeTest {
 
         SinglyLinkedList<Integer> singlyLinkedList = new SinglyLinkedList<>(head);
 
-        IntStream.range(1, 10)
-                .forEach(singlyLinkedList::add);
+        List<Integer> expected = IntStream.range(1, 10)
+                .sorted()
+                .boxed()
+                .collect(Collectors.toList());
+
+        expected.forEach(singlyLinkedList::add);
 
         Assertions.assertEquals(head, singlyLinkedList.getHead());
         Assertions.assertEquals(0, head.getValue());
         Assertions.assertEquals(10, singlyLinkedList.getSize());
         Assertions.assertNull(singlyLinkedList.getTail().getNext());
         Assertions.assertEquals(9, singlyLinkedList.getTail().getValue());
-
-        for (Node<Integer> node : singlyLinkedList) {
-            System.out.println(node.getValue() + ", " + node.getNext());
-        }
     }
 
 }
